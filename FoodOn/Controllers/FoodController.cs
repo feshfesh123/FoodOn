3d659f8957fcdf0d0f7305aa0319f17c3840895c
Ngoc Tai <ngoctai.nguyen234@gmail.com>
2019-12-10 16:27:55 +0700

    second

--- FoodOn/Controllers/FoodController.cs
+++ FoodOn/Controllers/FoodController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using FoodOn.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using VDS.RDF;
@@ -31,7 +32,7 @@ namespace FoodOn.Controllers
             query.Namespaces.AddNamespace("rdfs", new Uri("http://www.w3.org/2000/01/rdf-schema#"));
             query.Namespaces.AddNamespace("rdf", new Uri("http://www.w3.org/1999/02/22-rdf-syntax-ns#"));
             query.Namespaces.AddNamespace("food", new Uri("http://purl.org/heals/food/"));
-            query.Namespaces.AddNamespace("ingredient", new Uri("http://purl.org/heals/ingredient#"));
+            query.Namespaces.AddNamespace("ingredient", new Uri("http://purl.org/heals/ingredient/"));
             return query;
         }
 
@@ -59,18 +60,49 @@ namespace FoodOn.Controllers
             }
             return ingreList;
         }
+
+        private List<string> FindRecipes(List<string> ingreList)
+        {
+            List<string> recipeList = new List<string>();
+
+            SparqlParameterizedString query = AddPrefix();
+            query.CommandText = "SELECT DISTINCT ?recipe WHERE {";
+            foreach(var item in ingreList)
+            {
+                query.CommandText += "?recipe food:hasIngredient ingredient:" + item + ".";
+            }
+            query.CommandText += "}";
+
+            SparqlQuery queryEx = _parser.ParseFromString(query);
+            Object result = _graph.ExecuteQuery(queryEx);
+            if (result is SparqlResultSet)
+            {
+                String recipeLabel;
+                SparqlResultSet rset = (SparqlResultSet)result;
+                foreach (SparqlResult r in rset)
+                {
+                    INode n;
+                    if (r.TryGetValue("recipe", out n))
+                    {
+                        recipeLabel = ((IUriNode)n).Uri.Segments[3];
+                        recipeList.Add(recipeLabel);
+                    }
+                }
+            }
+            return recipeList;
+        }
         public IActionResult Index()
         {
             var ingreList = GetAllIngredients();
             ViewBag.Ingredients = new SelectList(ingreList);
-            return View();
+            return View(new SearchViewModel());
         }
         
         [HttpPost]
-        public IActionResult Index(string[] ingredientList)
+        public IActionResult Index(SearchViewModel model)
         {
-            var ingreList = ingredientList;
-            return View();
+            var result = FindRecipes(model.Ingredients);
+            return PartialView("_Result", result);
         }
     }
 }
---
+++ FoodOn/Models/SearchViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodOn.Models
+{
+    public class SearchViewModel
+    {
+        public enum Meal { All, Breakfast, Lunch, Dinner}
+        public enum Course { All, Dessert, Salad, Soup, LowSugar}
+        public List<string> Ingredients { get; set; } = new List<string>();
+        public int MealType { get; set; } = (int)Meal.All;
+        public int CourseType { get; set; } = (int)Course.All;
+
+    }
+}
